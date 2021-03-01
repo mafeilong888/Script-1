deb4d84afa48509f45399d136b42163da53805ef
wsdjj <76938752+wsdjj@users.noreply.github.com>
2021-03-01 06:46:25 +0000

    Update ysm.js

--- ysm.js
+++ ysm.js
@@ -184,7 +184,7 @@ function ysm1(timeout = 0) {
 let url = {
         url : "http:"+ysmurl.match(/http:(.*?)yunonline/)[1]+"yunonline/v1/task",
         headers : JSON.parse(ysmhd),
-        body : JSON.parse($.getdata('ysmbody'))
+        body : JSON.parse(ysmbody)
 }
       $.post(url, async (err, resp, data) => {
         try {
