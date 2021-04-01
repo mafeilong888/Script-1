af98e6bbcc1d1280ed374e0ad6f7f9c99c0c51da
wsdjj <76938752+wsdjj@users.noreply.github.com>
2021-04-01 04:48:54 +0000

    Update ysm.js

--- ysm.js
+++ ysm.js
@@ -66,7 +66,7 @@ const baseHeaders = {
   "X-Requested-With": "XMLHttpRequest"
 }
 if ($.isNode()) {
-   ysm = process.env.ysm
+ 
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
   
