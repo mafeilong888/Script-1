b4d26c72c1fabae01c38552bd89593f649c285d1
wsdjj <76938752+wsdjj@users.noreply.github.com>
2021-05-11 01:28:24 +0000

    Create xmCOOKIE.js

---
+++ xmCOOKIE.js
@@ -0,0 +1,22 @@
+  
+/* ziye
+独立COOKIE文件
+github地址 https://github.com/ziye888
+TG频道地址  https://t.me/ziyescript
+TG交流群   https://t.me/joinchat/AAAAAE7XHm-q1-7Np-tF3g
+boxjs链接  https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/ziye.boxjs.json
+圈X task订阅 https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/ziye-gallery.json
+转载请备注个名字，谢谢
+在boxjs里选择复制会话 粘贴至cookie处
+*/
+
+
+
+
+let cookie = {"id":"xm","name":"小芒APP","keys":["xmurl","xmurl2","xmurl3","xmurl4","xmurl5","xmurl6","xmurl7","xmurl8","xmurl9","xmurl10","xmurl11","xmurl12","xmurl13","xmurl14","xmurl15","xmurl16","xmurl17","xmurl18","xmurl19","xmurl20"],"author":"@ziye","settings":[{"id":"xmSuffix","name":"当前账号","val":"1","type":"number","desc":"当前抓取ck记录的账号序号，如：1、2、3、"},{"id":"xmCount","name":"账号个数","val":"1","type":"number","desc":"指定任务最多跑几个账号，根据抓取的账号数据个数来设值"},{"id":"xmXH","name":"循环获取CK","val":"0","type":"number","desc":"0关闭 1开启,默认关闭"},{"id":"xmXYZ","name":"执行概率","val":"100","type":"number","desc":"0不执行 可设置0-100,默认百分百"},{"id":"shuqinotifyttt","name":"推送控制","val":"1","type":"number","desc":"0关闭，1推送,默认12点以及23点推送"},{"id":"shuqinotifyInterval","name":"通知控制","val":"2","type":"number","desc":"0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知 "},{"id":"shuqiMinutes","name":"推送-通知 分钟控制","val":"10","type":"number","desc":"推送以及通知控制在什么分钟段，可设置0-59,默认0到10"}],"repo":"https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/xm.js","icons":["https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/xm.png","https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/xm.png"],"script":"https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/xm.js","icon":"https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/xm.png","favIcon":"mdi-star-outline","favIconColor":"grey","datas":[{"key":"xmurl","val":"https://mgesq.api.mgtv.com/user/center?dysign=x2nN4wOe6bTIzwUBRQjYmCN3ep17GB3XjAwKe6BrW4hDwpnC%2Foywi5EVfOK08NtndJh0S8bqprBhXG%2BlR4w1s9Nwelc701kwHo2H6ORuZGMVDtL2&appVersion=4.2.2&device=iPhone&did=e8acddc67a6afaca62a1852ee88da95485767111&dname=iPhone&isEcomVip=0&isVip=1&mac=e8acddc67a6afaca62a1852ee88da95485767111&osType=ios&osVersion=14.4&seqId=1e0d089c36a7bed95ba1a87c41a3e7a8&ticket=C2CRFK8KJ9J09HAV9BH0&uuid=8421b0817e254b88abf069c8c67e527e"},{"key":"xmurl2","val":""},{"key":"xmurl3","val":""},{"key":"xmurl4","val":""},{"key":"xmurl5","val":""},{"key":"xmurl6","val":""},{"key":"xmurl7","val":""},{"key":"xmurl8","val":""},{"key":"xmurl9","val":""},{"key":"xmurl10","val":""},{"key":"xmurl11","val":""},{"key":"xmurl12","val":""},{"key":"xmurl13","val":""},{"key":"xmurl14","val":""},{"key":"xmurl15","val":""},{"key":"xmurl16","val":""},{"key":"xmurl17","val":""},{"key":"xmurl18","val":""},{"key":"xmurl19","val":""},{"key":"xmurl20","val":""}],"sessions":[],"isLoaded":true}
+
+
+
+
+
+module.exports = cookie
